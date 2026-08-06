0f7f9bfeef096d793971ba12d7bd7b79cb3d6bc7
Emil61 <emilcomarazamy@gmail.com>
2026-08-05 21:39:13 -0400

    pc

--- services/inventario.py
+++ services/inventario.py
@@ -1,34 +1,37 @@
-class inventario:
+class Inventario:
+
 
     def __init__(self):
         """
-        Aquí represento el inventario en una estructura de lista, porque me interesa
-        el orden de inserción.
+            Aqui represento el inventario en una estructra de lista, porque me interesa
+            e orden de inserción.
         """
         self.productos = []
 
-    def registrar_producto(self, producto):
+    def registrar_producto(self):
         """
-        Permite agregar productos al inventario.
+            Permite agregar productos al inventario.
         """
         print("\n[Inventario]")
         print("Registrar producto (pendiente).")
 
     def listar_productos(self):
-  
+
         print("\n[Inventario]")
         print("Listar productos (pendiente).")
 
-    def buscar_producto(self, nombre):
+    def buscar_producto(self):
         print("\n[Inventario]")
         print("Buscar producto (pendiente).")
 
-    def eliminar_producto(self, nombre):
+    def eliminar_producto(self):
         print("\n[Inventario]")
         print("Eliminar producto (pendiente).")
 
     def mostrar_cantidad_productos(self):
         """
-        Este método se puede implementar si necesitamos conocer cuantos productos tenemos en inventario.
+            Este método se puede implementar si necesitamos conocer cuantos productos tenemos en inventario.
         """
         print("mostrar cantidad de productos (pendiente).")
+
+    
