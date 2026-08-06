81e00e57a05d84495b90256d724bb050e6a9c8f1
Emil61 <emilcomarazamy@gmail.com>
2026-08-05 21:15:29 -0400

    pr

---
+++ models/venta.py
@@ -0,0 +1,19 @@
+class Venta:
+    def __init__(self):
+        """
+            Constuctor de la clase Venta
+        """
+        pass
+
+
+    def calcular_total(self):
+        """
+            Método para calcular el total de la venta
+        """
+        print("Método calcular_total() pendiente")
+
+    def mostrar(self):
+        """
+            Método para mostrar los detalles de la venta
+        """
+        print("Método mostrar() pendiente ")
---
+++ services/inventario.py
@@ -0,0 +1,34 @@
+class inventario:
+
+    def __init__(self):
+        """
+        Aquí represento el inventario en una estructura de lista, porque me interesa
+        el orden de inserción.
+        """
+        self.productos = []
+
+    def registrar_producto(self, producto):
+        """
+        Permite agregar productos al inventario.
+        """
+        print("\n[Inventario]")
+        print("Registrar producto (pendiente).")
+
+    def listar_productos(self):
+  
+        print("\n[Inventario]")
+        print("Listar productos (pendiente).")
+
+    def buscar_producto(self, nombre):
+        print("\n[Inventario]")
+        print("Buscar producto (pendiente).")
+
+    def eliminar_producto(self, nombre):
+        print("\n[Inventario]")
+        print("Eliminar producto (pendiente).")
+
+    def mostrar_cantidad_productos(self):
+        """
+        Este método se puede implementar si necesitamos conocer cuantos productos tenemos en inventario.
+        """
+        print("mostrar cantidad de productos (pendiente).")
